8d11bf702c3877e99d2d1c1a7921067a720bd949
dw1b_alum12 <dw1b_alum12@DW1B-02.inform.local>
2019-05-24 13:22:34 +0200

    Subir Modificaciones paso 4

---
+++ Ejercicio6/src/ejercicio6/Casa.java
@@ -0,0 +1,13 @@
+package ejercicio6;
+
+public class Casa {
+
+	/**
+	 * Un equipo de limpiadores deja la casa libre de germenes
+	 * y definfecta todas las superficies
+	 */
+	public void limpiarLaCasa() {
+		String mensajito="Te hemos limpiado la casa, esta desinfectada";
+		System.out.println(mensajito);
+	}
+}
