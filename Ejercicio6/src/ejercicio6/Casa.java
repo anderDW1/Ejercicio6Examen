6843a8e9c3316af69edd40ca1f206941b962e387
dw1b_alum12 <dw1b_alum12@DW1B-02.inform.local>
2019-05-24 13:30:54 +0200

    Va a entrar en clonflicto

--- Ejercicio6/src/ejercicio6/Casa.java
+++ Ejercicio6/src/ejercicio6/Casa.java
@@ -2,12 +2,20 @@ package ejercicio6;
 
 public class Casa {
 
+	private float superficie;
+	private String calle;
+	private int codigoPostal;
+	
 	/**
 	 * Un equipo de limpiadores deja la casa libre de germenes
 	 * y definfecta todas las superficies
 	 */
 	public void limpiarLaCasa() {
-		String mensajito="Te hemos limpiado la casa, esta desinfectada";
+		mensaje();
+	}
+
+	private void mensaje() {
+		String mensajito="Te hemos limpiado la casa, esta desinfectada y nadie te ha robado";
 		System.out.println(mensajito);
 	}
 }
